fc9e1da4e639c253aa4cc986f2bce42811394241
Christopher Rhodes <chrhodes@cox.net>
2018-02-26 15:30:57 -0800

    Added acolade

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("hello world");
+			Console.WriteLine("Shirin is in the house!");
             Console.ReadLine();
         }
     }
